acf85a745cf80933721cac04fab86a7d5eba25ad
i14kwmr <i14kwmr@gmail.com>
2021-10-08 10:07:13 +0900

    add file: TransformerEncoder.py

---
+++ TransformerEncoder.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+
+from EncoderBlock import EncoderBlock
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, num_layers, **block_args):
+        super().__init__()
+        self.layers = nn.ModuleList([EncoderBlock(**block_args) for _ in range(num_layers)])  # 層を重ねる
+
+    def forward(self, x, mask=None):
+        for layer in self.layers:
+            x = layer(x, mask=mask)
+        return x
+
+    def get_attention_maps(self, x, mask=None):  # attention mapのみ取得
+        attention_maps = []
+        for layer in self.layers:
+            _, attn_map = layer.self_attn(x, mask=mask, return_attention=True)
+            attention_maps.append(attn_map)
+            x = layer(x)  # 次の層へ
+        return attention_maps
